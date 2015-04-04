711d45928447528bfc3d0917314d261ae909320b
Keith Conger <keith.conger@gmail.com>
2015-04-03 22:30:40 -0600

    Patches: *Fix nlink type *Remove check for emulator for bluetooth *Show power on all the time

--- patches/patch.sh
+++ patches/patch.sh
@@ -9,3 +9,18 @@ cd system/core
 patch -p1 < ../../device/ti/beagleboneblack/patches/0001-Fix-CallStack-API.patch
 cd ../..
 
+# Bionic patch to fix nlink type
+# https://android.googlesource.com/platform/bionic.git/+/ff5b0fc8f3406c98561e17bd2a6c5433e192f5be%5E1..ff5b0fc8f3406c98561e17bd2a6c5433e192f5be/
+cd bionic
+patch -p1 < ../../device/ti/beagleboneblack/patches/0001-Fix-nlink-type.patch
+cd ../
+
+# Remove emulator check for bluetooth since we need qemu=1
+cd frameworks/base 
+patch -p1 < ../../device/ti/beagleboneblack/patches/SystemServer.java.patch
+cd ../..
+
+# Show battery icon as always powered
+cd system/core
+patch -p1 < ../../device/ti/beagleboneblack/patches/0008-ENGR00289825-Make-simulated-battery-status-report-ba.patch
+cd ../..
